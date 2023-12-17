5c78adcae169fc6b3ec3d9adeadc8afaa4d09f13
Wendel Luan <wendelluan@alu.ufc.br>
2023-12-17 12:40:40 -0300

    Header Update

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import classes from './Header.module.css'
+import classes from "./Header.module.css";
 
-import HeaderCartButton from './HeaderCartButton'
+import HeaderCartButton from "./HeaderCartButton";
 
 export default function Header(props) {
   return (
     <>
-        <header className={classes.header}>
-            <h1>ReactMeals</h1>
-            <HeaderCartButton onClick={props.onShowModal} />
-        </header>
+      <header className={classes.header}>
+        <h1>DeliveryMeals</h1>
+        <HeaderCartButton onClick={props.onShowModal} />
+      </header>
 
-        <div className={classes['main-image']}>
-            <img src="/assets/meals.jpg" alt="Table with a lot of foods" />
-        </div>
+      <div className={classes["main-image"]}>
+        <img src="/assets/meals.jpg" alt="Table with a lot of foods" />
+      </div>
     </>
-  )
+  );
 }
